f081c797f89c1698469bc6a63663c8844b5d3cd8
dionkb <dionkbaskara@gmail.com>
2023-07-18 12:36:25 -0400

    multiple project cards now added

--- src/utils/dataArrays.js
+++ src/utils/dataArrays.js
@@ -1,4 +1,10 @@
 import StarGazers from '../assets/StarGazers.png'
+import EmployeeSQLDatabase from '../assets/EmployeeSQLDatabase.jpg'
+import MVCTechBlog from '../assets/MVCTechBlog.jpg'
+import READMEGenerator from '../assets/READMEGenerator.jpg'
+import GistTutorial from '../assets/GistTutorial.png'
+import OldPortfolio from '../assets/OldPortfolio.jpg'
+
 
 const projectData = [
     {
@@ -6,8 +12,54 @@ const projectData = [
         image: StarGazers,
         description: "A web application to help you find the best times and places to go stargazing!",
         workType: "Group",
-        deployedURL: "https://sabplpz.github.io/StarGazers",
+        variableBtnText: "View App",
+        applicationURL: "https://sabplpz.github.io/StarGazers",
         repositoryURL: "https://github.com/Sabplpz/StarGazers.git"
+    },
+    {
+        title: "Employee Database using SQL",
+        image: EmployeeSQLDatabase,
+        description: "This command-line-application is an employee management database using SQL",
+        workType: "Solo",
+        variableBtnText: "Executing The App",
+        applicationURL: "https://github.com/dionkb/vigilant-fishstick.git#execution",
+        repositoryURL: "https://github.com/dionkb/vigilant-fishstick.git"
+    },
+    {
+        title: "MVC Tech Blog",
+        image: MVCTechBlog,
+        description: "A basic MVC structured blog to showcase skills with using CRUD operations, basic encryption, session-variables, and more.",
+        workType: "Solo",
+        variableBtnText: "View App",
+        applicationURL: "https://sheltered-badlands-65127.herokuapp.com/",
+        repositoryURL: "https://github.com/dionkb/stunning-octo-dollop.git"
+    },
+    {
+        title: "README Generator",
+        image: READMEGenerator,
+        description: "Answer a series of prompts that will then be used to populate a README file that is newly generated. There are several questions that alllow the user to customize the README based on their needs.",
+        workType: "Solo",
+        variableBtnText: "Executing The App",
+        applicationURL: "https://github.com/dionkb/ubiquitous-octo-invention#execution",
+        repositoryURL: "https://github.com/dionkb/ubiquitous-octo-invention"
+    },
+    {
+        title: "Gist Tutorial",
+        image: GistTutorial,
+        description: "I also plan on helping others learn and contributing to open-source apps once I have more experience, just like this Gist tutorial shows",
+        workType: "Solo",
+        variableBtnText: "View Gist",
+        applicationURL: "https://gist.github.com/dionkb/7807b363e16dd03c36ce2f60c782a856",
+        repositoryURL: "https://gist.github.com/dionkb/7807b363e16dd03c36ce2f60c782a856"
+    },
+    {
+        title: "My Old HTML/CSS Portfolio ❤",
+        image: OldPortfolio,
+        description: "My old HTML/CSS-only portfolio. Let's see how far I've come, and how far I'll go.",
+        workType: "Solo",
+        variableBtnText: "View Page",
+        applicationURL: "https://dionkb.github.io/Dion-Baskara-Portfolio/",
+        repositoryURL: "https://github.com/dionkb/Dion-Baskara-Portfolio.git"
     },
 ];
 
