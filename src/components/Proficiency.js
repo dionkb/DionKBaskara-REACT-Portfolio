50293aeae24f7416e0c1492f83619bf438a6bf08
dionkb <dionkbaskara@gmail.com>
2023-07-18 17:51:14 -0400

    Approaching MVP level of development

---
+++ src/components/Proficiency.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Proficiency(props) {
+    return (
+        <div>
+            <div class="proficienciesList">
+                <p>{props.name}</p>
+            </div>
+        </div>  
+    );
+}
+
+export default Proficiency;
